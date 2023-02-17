42ff6e7444cb34b39b2ea08a0ca2aa54d6d0d3fc
hedy_cheng <hedy_cheng@gss.com.tw>
2023-02-17 16:06:11 +0800

    feat(data): add book and categories data

---
+++ src/app/data.categories.ts
@@ -0,0 +1,7 @@
+export const categories: Array<{ text: string; value: string }> = [
+  {text:"資料庫", value:""},
+  {text:"網際網路", value:""},
+  {text:"應用系統整合", value:""},
+  {text:"家庭保健", value:""},
+  {text:"語言", value:""}
+];
